8384ce86be864f5564faa8efedc1196648aa0dbb
Emanuele Pace <emanuele.10@outlook.it>
2021-02-09 10:05:15 +0000

    Refactoring and let the code be as much DRY as possible

--- tenMinuteWalk.js
+++ tenMinuteWalk.js
@@ -1,17 +1,18 @@
-function walk(directions){
+function isTenMinuteWalk(directions){
 
-    const timesOfNorth = directions.filter(word => word ==="n")
-    const timeOfSouth = directions.filter(word => word ==="s")
-    const timeOfWest = directions.filter(word => word ==="w")
-    const timeOfEast = directions.filter(word => word ==="e")
+    const timesOfNorth = (directions.filter(word => word ==="n")).length
+    const timeOfSouth = (directions.filter(word => word ==="s")).length
+    const timeOfWest = (directions.filter(word => word ==="w")).length
+    const timeOfEast = (directions.filter(word => word ==="e")).length
 
     if (directions.length !== 10 ){
         return false
-    } else if ((timesOfNorth.length == timeOfSouth.length) && (timeOfWest.length == timeOfEast.length)){
+    } else if ((timesOfNorth == timeOfSouth) && (timeOfWest == timeOfEast)){
         return true
-    } else if ((timesOfNorth.length !== timeOfSouth.length) || (timeOfWest.length !== timeOfEast.length)){
+    } else {
+        (timesOfNorth !== timeOfSouth) || (timeOfWest !== timeOfEast);
         return false
     }
 }
 
-module.exports = walk;
+module.exports = isTenMinuteWalk;
--- tenMinuteWalk.test.js
+++ tenMinuteWalk.test.js
@@ -1,30 +1,30 @@
-const walk = require("./tenMinuteWalk")
+const isTenMinuteWalk = require("./tenMinuteWalk")
 
 describe('Ten Minutes Walk', () =>{
-    it('Return FALSE if the array is composed by just one elements', () =>{
-        expect(walk(["n"])).toBe(false)
+    xit('Return FALSE if the array is composed by just one elements', () =>{
+        expect(isTenMinuteWalk(["n"])).toBe(false)
     })
     it('Return FALSE if the array is composed by just two elements', () =>{
-        expect(walk(["w", "s"])).toBe(false)
+        expect(isTenMinuteWalk(["w", "s"])).toBe(false)
     })
-    it('Return FALSE if the array is composed by just 9 elements', () =>{
-        expect(walk(['w', 's', 'e', 's', 's', 'e', 's', 'w', 'n'])).toBe(false)
+    xit('Return FALSE if the array is composed by just 9 elements', () =>{
+        expect(isTenMinuteWalk(['w', 's', 'e', 's', 's', 'e', 's', 'w', 'n'])).toBe(false)
     })
     xit('Return TRUE if the array is composed by 10 elements', () =>{
-        expect(walk(['w', 's', 'e', 's', 's', 'e', 's', 'w', 'n','e'])).toBe(true)
+        expect(isTenMinuteWalk(['w', 's', 'e', 's', 's', 'e', 's', 'w', 'n','e'])).toBe(true)
     })
 
     xit('Check if the numbers of "n" are equals to number of "s"', () => {
-        expect(walk(['n', 's', 'e', 's', 's', 'e', 's', 'w', 'e','n'])).toBe("Same numbers of 'n' and 's'")
+        expect(isTenMinuteWalk(['n', 's', 'e', 's', 's', 'e', 's', 'w', 'e','n'])).toBe("Same numbers of 'n' and 's'")
     })
     it('Return TRUE if number of n/s are the same of e/w', () => {
-        expect(walk(['w', 's', 'e', 'e', 'n', 'n', 'e', 's', 'w', 'w'])).toBe(true)
+        expect(isTenMinuteWalk(['w', 's', 'e', 'e', 'n', 'n', 'e', 's', 'w', 'w'])).toBe(true)
     })
     it('Return FALSE if number of n/s are not the same of e/w', () => {
-        expect(walk(['w', 'w', 'w', 'e', 'n', 'n', 'e', 's', 'w', 'w'])).toBe(false)
+        expect(isTenMinuteWalk(['w', 'w', 'w', 'e', 'n', 'n', 'e', 's', 'w', 'w'])).toBe(false)
     })
     it('Return TRUE if number of n/s are the same of e/w', () => {
-        expect(walk(['w','w', 'e', 'e', 'n', 'n', 'e', 'w', 's', 's'])).toBe(true)
+        expect(isTenMinuteWalk(['w','w', 'e', 'e', 'n', 'n', 'e', 'w', 's', 's'])).toBe(true)
     })
 
 
